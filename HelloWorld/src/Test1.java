2dc9a8f7bb159863609f23c559f03d419f36fad7
Amar <Amar@DESKTOP-31982KI>
2020-10-05 18:29:00 +0530

    All commited

---
+++ HelloWorld/src/Test1.java
@@ -0,0 +1,10 @@
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.print("Hello world");
+System.out.print("My name is Aparna");
+	}
+
+}
